aa7f6963caad31d506870a1ba57d950d8899d38b
Valentina Diego <valentinadiego90@gmail.com>
2023-10-26 17:50:37 -0300

    Se cambiaron los archivos  a los de la pre entrega 2

--- js/main.js
+++ js/main.js
@@ -1,78 +1,208 @@
-//FUNCIONES
-function bienvenida(inicio) {
-    while (inicio !== 0 && inicio !== 1) {
-        alert("Ingrese una opción válida");
-        inicio = parseInt(prompt("¡Bienvenido a Candelitte! ¿Desea comprar nuestros productos? Escriba 1 para comprar, 0 para salir."));
+// Funciones / Clases(objetos)
+class Producto {
+    constructor(nombre, precio, stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
     }
-    if (inicio === 1) {
-        agregarAlCarrito();
-    } else if (inicio === 0){
-        alert("Muchas gracias por pasar");
+
+    mostrarProducto() {
+        alert("El producto seleccionado es " + this.nombre + ", el precio es de $" + this.precio + " y hay un stock de " + this.stock + " unidades.");
     }
 }
 
-function calcularTotal() {
-    total = (precioTorta * totalUnidadesTorta) + (precioBrownie * totalUnidadesBrownie) + (precioGalletitas * totalUnidadesGalletitas);
-    return total;
+function restaStock(stock){
+    return stock - totalUnidades;
 }
 
-function agregarAlCarrito() {
-    let productos = parseInt(prompt("Ingrese el numero del producto que desea comprar: 1- Torta $1500. 2- Brownie $1000. 3- Galletitas $500. Pulse 0 para terminar la compra."));
+function fcAgregarAlCarrito() {
+    let producto = prompt("Ingrese el nombre del producto que desea agregar al carrito. Presione 0 para volver atrás.");
+    while(producto !== "0"){
+        if (producto.toLowerCase() == "torta block" || producto.toLowerCase() == "torta cadbury" || producto.toLowerCase() == "chocotorta" || producto.toLowerCase() == "cheesecake" || producto.toLowerCase() == "torta alimonada" || producto.toLowerCase() == "torta snickers"){
+            switch(producto){
+                case "torta block":
+                    precio = tortas[0].precio;
+                    unidades = parseInt(prompt("Ingrese cuántas unidades desea agregar."));
+                        if (unidades > tortas[0].stock){
+                            alert("No contamos con suficiente stock de ese producto.");
+                        } else if (unidades <= 0){
+                            alert("Ingrese un valor mayor a 0.");
+                        }
+                    while(unidades > 0 && unidades <= tortas[0].stock){
+                        alert("Agregaste " + unidades + " unidades de " + tortas[0].nombre + ".");
+                        totalUnidades += unidades;
+                        restaStock(tortas[0].stock);
+                        carrito.push({producto,unidades,precio});
+                        break;
+                    }
+                    break;
+                case "torta cadbury":
+                    precio = tortas[1].precio;
+                    unidades = parseInt(prompt("Ingrese cuántas unidades desea agregar."));
+                        if (unidades > tortas[1].stock){
+                            alert("No contamos con suficiente stock de ese producto.");
+                        } else if (unidades <= 0){
+                            alert("Ingrese un valor mayor a 0.");
+                        }
+                    while(unidades > 0 && unidades <= tortas[1].stock){
+                        alert("Agregaste " + unidades + " unidades de " + tortas[1].nombre + ".");
+                        totalUnidades += unidades;
+                        restaStock(tortas[1].stock);
+                        carrito.push({producto,unidades,precio});
+                        break;
+                    }
+                    break;
+                case "chocotorta":
+                    precio = tortas[2].precio;
+                    unidades = parseInt(prompt("Ingrese cuántas unidades desea agregar."));
+                        if (unidades > tortas[2].stock){
+                            alert("No contamos con suficiente stock de ese producto.");
+                        } else if (unidades <= 0){
+                            alert("Ingrese un valor mayor a 0.");
+                        }
+                    while(unidades > 0 && unidades <= tortas[2].stock){
+                        alert("Agregaste " + unidades + " unidades de " + tortas[2].nombre + ".");
+                        totalUnidades += unidades;
+                        restaStock(tortas[2].stock);
+                        carrito.push({producto,unidades,precio});
+                        break;
+                    }
+                    break;
+                case "cheesecake":
+                    precio = tortas[3].precio;
+                    unidades = parseInt(prompt("Ingrese cuántas unidades desea agregar."));
+                        if (unidades > tortas[3].stock){
+                            alert("No contamos con suficiente stock de ese producto.");
+                        } else if (unidades <= 0){
+                            alert("Ingrese un valor mayor a 0.");
+                        }
+                    while(unidades > 0 && unidades <= tortas[3].stock){
+                        alert("Agregaste " + unidades + " unidades de " + tortas[3].nombre + ".");
+                        totalUnidades += unidades;
+                        restaStock(tortas[3].stock);
+                        carrito.push({producto,unidades,precio});
+                        break;
+                    }
+                    break;
+                case "torta alimonada":
+                    precio = tortas[4].precio;
+                    unidades = parseInt(prompt("Ingrese cuántas unidades desea agregar."));
+                        if (unidades > tortas[4].stock){
+                            alert("No contamos con suficiente stock de ese producto.");
+                        } else if (unidades <= 0){
+                            alert("Ingrese un valor mayor a 0.");
+                        }
+                    while(unidades > 0 && unidades <= tortas[4].stock){
+                        alert("Agregaste " + unidades + " unidades de " + tortas[4].nombre + ".");
+                        totalUnidades += unidades;
+                        restaStock(tortas[4].stock);
+                        carrito.push({producto,unidades,precio});
+                        break;
+                    }
+                    break;
+                case "torta snickers":
+                    precio = tortas[5].precio;
+                    unidades = parseInt(prompt("Ingrese cuántas unidades desea agregar."));
+                        if (unidades > tortas[5].stock){
+                            alert("No contamos con suficiente stock de ese producto.");
+                        } else if (unidades <= 0){
+                            alert("Ingrese un valor mayor a 0.");
+                        }
+                    while(unidades > 0 && unidades <= tortas[5].stock){
+                        alert("Agregaste " + unidades + " unidades de " + tortas[5].nombre + ".");
+                        totalUnidades += unidades;
+                        restaStock(tortas[5].stock);
+                        carrito.push({producto,unidades,precio});
+                        break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            alert("Producto incorrecto. Asegúrese de ingresar bien el nombre del producto.")
+        }
+        producto = prompt("Ingrese el nombre del producto que desea agregar al carrito. Presione 0 para volver atrás.");
+    }
+}
 
-    while (productos !== 0) {
-        switch (productos) {
+function comprar() {
+    const mostrarTortas = tortas.map((producto) => producto.nombre + " $" + producto.precio);
+    alert("Nuestra lista de productos:\nSECCIÓN TORTAS:\n" + mostrarTortas.join(" - "));
+    let operacionComprar = parseInt(prompt("Ingrese la operación que desea realizar:\n1- AGREGAR UN PRODUCTO AL CARRITO\n2- CONSULTAR MI CARRITO\n3- FINALIZAR LA COMPRA\n4- VOLVER A VER LOS PRODUCTOS\n0- VOLVER AL INICIO"));
+    while(operacionComprar !== 0){
+        switch(operacionComprar){
             case 1:
-                unidadesTorta = parseInt(prompt("El producto seleccionado es Torta. Cuántas unidades quiere llevar?"));
-                while (unidadesTorta <= 0){
-                    alert("Ingrese una cantidad válida.")
-                    unidadesTorta = parseInt(prompt("El producto seleccionado es Torta. Cuántas unidades quiere llevar?"));
-                }
-                alert("Agregaste " + unidadesTorta + " unidades de Torta");
-                totalUnidadesTorta += unidadesTorta;
-
+                fcAgregarAlCarrito();
                 break;
             case 2:
-                unidadesBrownie = parseInt(prompt("El producto seleccionado es Brownie. Cuántas unidades quiere llevar?"));
-                while (unidadesBrownie <= 0){
-                    alert("Ingrese una cantidad válida.")
-                    unidadesBrownie = parseInt(prompt("El producto seleccionado es Brownie. Cuántas unidades quiere llevar?"));
+                if(carrito.length > 0){
+                    const consultarCarrito = carrito.map((producto) => producto.unidades + " unidades de " + producto.producto + " a $" + producto.precio * producto.unidades);
+                    alert("Su carrito:\n" + consultarCarrito.join("\n"));
+                } else {
+                    alert("Su carrito está vacío. Agregue productos para poder comprar.");
                 }
-                alert("Agregaste " + unidadesBrownie + " unidades de Brownie");
-                totalUnidadesBrownie += unidadesBrownie;
-
                 break;
             case 3:
-                unidadesGalletitas = parseInt(prompt("El producto seleccionado es Galletitas. Cuántas unidades quiere llevar?"));
-                while (unidadesGalletitas <= 0){
-                    alert("Ingrese una cantidad válida.")
-                    unidadesGalletitas = parseInt(prompt("El producto seleccionado es Galletitas. Cuántas unidades quiere llevar?"));
+                if(carrito.length > 0){
+                    alert("¡Compra finalizada! Podrá visualizar el resumen de su compra en la consola al salir del programa. ¡Hasta la próxima!");
+                    carrito.forEach((carritoFinal) => {
+                        console.log(carritoFinal.unidades + " unidades de " + carritoFinal.producto + " a $" + carritoFinal.precio * carritoFinal.unidades);
+                    });
+                    const total = carrito.reduce((acc,el) => acc + el.precio * el.unidades,0);
+                    console.log("El total de su compra es de $" + total + ".");
+                } else {
+                    alert("Su carrito está vacío. Agregue productos para poder comprar.");
                 }
-                alert("Agregaste " + unidadesGalletitas + " unidades de Galletitas");
-                totalUnidadesGalletitas += unidadesGalletitas;
-
+                break;
+            case 4:
+                alert("Nuestra lista de productos:\nSECCIÓN TORTAS:\n" + mostrarTortas.join(" - "));
                 break;
             default:
-                alert("Ingrese un valor válido");
+                alert("Ingrese una operación válida.");
                 break;
         }
-        productos = parseInt(prompt("Ingrese el numero del producto que desea comprar: 1- Torta $1500. 2- Brownie $1000. 3- Galletita $500. Pulse 0 para terminar la compra."));
+        operacionComprar = parseInt(prompt("Ingrese la operación que desea realizar:\n1- AGREGAR UN PRODUCTO AL CARRITO\n2- CONSULTAR MI CARRITO\n3- FINALIZAR LA COMPRA\n4- VOLVER A VER LOS PRODUCTOS\n0- VOLVER AL INICIO"));
     }
-    alert("Su carrito:\n" + totalUnidadesTorta + " unidades de Torta a $" + (precioTorta * totalUnidadesTorta) + ". \n"+ totalUnidadesBrownie + " unidades de Brownie a $" + (precioBrownie * totalUnidadesBrownie) + ". \n"+ totalUnidadesGalletitas + " unidades de Galletitas a $" + (precioGalletitas * totalUnidadesGalletitas) + ".\nEl total a abonar es de $" + calcularTotal() + ". ¡Muchas gracias por su compra!");
 }
 
+// Variables
+const tortas = [
+    new Producto("Torta Block", 1450, 5),
+    new Producto("Torta Cadbury", 1350, 8),
+    new Producto("Chocotorta", 1250, 6),
+    new Producto("Cheesecake", 1150, 9),
+    new Producto("Torta Alimonada", 1450, 4),
+    new Producto("Torta Snickers", 1550, 10),
+];
 
-//VARIABLES
-let precioTorta = 1500;
-let precioBrownie = 1000;
-let precioGalletitas = 500;
-let unidadesTorta = 0;
-let unidadesBrownie = 0;
-let unidadesGalletitas = 0;
-let totalUnidadesTorta = 0;
-let totalUnidadesBrownie = 0;
-let totalUnidadesGalletitas = 0;
+const carrito = [];
+let precio = tortas[0].precio;
+let unidades = 0;
+let totalUnidades = 0;
 
+// Inicio del programa
+let operacion = parseInt(prompt("¡Bienvenido a Candelitte! Ingrese la operación que desea realizar:\n1- COMPRAR\n2- VER LOS PRODUCTOS\n0- SALIR"));
 
-//INICIO DEL CÓDIGO
-let inicio = parseInt(prompt("¡Bienvenido a Candelitte! ¿Desea comprar nuestros productos? Escriba 1 para comprar, 0 para salir."));
-bienvenida(inicio);
+while(operacion !== 0){
+    switch(operacion){
+        case 1:
+            comprar();
+            break;
+
+        case 2:
+            const mostrarTortas = tortas.map((producto) => producto.nombre + " $" + producto.precio);
+            alert("Nuestra lista de productos:\nSECCIÓN TORTAS:\n" + mostrarTortas.join(" - "));
+            break;
+            
+        default:
+            alert("Ingrese una operación válida.");
+            break;
+    }
+    operacion = parseInt(prompt("¡Bienvenido a Candelitte! Ingrese la operación que desea realizar:\n1- COMPRAR\n2- VER LOS PRODUCTOS\n0- SALIR"));
+}
+if(operacion === 0){
+    alert("Muchas gracias por entrar a nuestra web, ¡hasta la próxima!");
+}
+
+console.log(carrito);
