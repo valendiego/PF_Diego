703e67d66a956026e2f26ec4762a8748b1706d22
Valentina Diego <valentinadiego90@gmail.com>
2023-11-08 00:12:35 -0300

    Se trabaja con la segunda pre entrega

--- js/app1.js
+++ js/app1.js
@@ -1,16 +1,17 @@
 document.getElementById("carrito__icon").addEventListener("click", mostrar__carrito);
 
-document.getElementById("background__menu").addEventListener("click", ocultar__carrito);
+document.getElementById("cerrar__carrito").addEventListener("click", ocultar__carrito);
 
 carritoDeCompras = document.getElementById("carrito");
-background__menu = document.getElementById("background__menu");
+cerrarCarrito = document.getElementById("cerrar__carrito");
+backgroundMenu = document.getElementById("background__menu");
 
 function mostrar__carrito(){
     carritoDeCompras.style.right = "0px";
-    background__menu.style.display = "block";
+    backgroundMenu.style.display = "block";
 }
 
 function ocultar__carrito(){
     carritoDeCompras.style.right = "-460px";
-    background__menu.style.display = "none";
+    backgroundMenu.style.display = "none";
 }
