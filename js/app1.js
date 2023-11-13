038ef0b5bb3f7f6a33e1f1730045883d4da5e765
Valentina Diego <valentinadiego90@gmail.com>
2023-11-13 01:09:14 -0300

    Se arregló el código para cerrar el carrito

--- js/app1.js
+++ js/app1.js
@@ -1,16 +1,14 @@
 document.getElementById("carrito__icon").addEventListener("click", mostrar__carrito);
 
-document.getElementById("background__menu").addEventListener("click", ocultar__carrito);
+document.getElementById("cerrar__carrito").addEventListener("click", ocultar__carrito);
 
 carritoDeCompras = document.getElementById("carrito");
-background__menu = document.getElementById("background__menu");
+cerrarCarrito = document.getElementById("cerrar__carrito");
 
 function mostrar__carrito(){
     carritoDeCompras.style.right = "0px";
-    background__menu.style.display = "block";
 }
 
 function ocultar__carrito(){
     carritoDeCompras.style.right = "-460px";
-    background__menu.style.display = "none";
 }
